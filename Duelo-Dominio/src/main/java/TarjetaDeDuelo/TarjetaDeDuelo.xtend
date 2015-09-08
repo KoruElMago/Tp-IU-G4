70bda2c6b4dfb02bcee517b9d199fedb541074f2
KoruElMago <javier.corbalan93@gmail.com>
2015-09-08 19:31:22 -0300

    actualizando

--- Duelo-Dominio/src/main/java/TarjetaDeDuelo/TarjetaDeDuelo.xtend
+++ Duelo-Dominio/src/main/java/TarjetaDeDuelo/TarjetaDeDuelo.xtend
@@ -1,8 +1,14 @@
 package TarjetaDeDuelo
 
+import Personaje.Personaje
+import Jugador.Jugador
+
 class TarjetaDeDuelo {
 	
 	var String linea
 	var Personaje personaje
 	var Jugador   jugador
-}
+	
+	
+	
+	}
--- Duelo-Dominio/src/main/xtend-gen/TarjetaDeDuelo/TarjetaDeDuelo.java
+++ Duelo-Dominio/src/main/xtend-gen/TarjetaDeDuelo/TarjetaDeDuelo.java
@@ -1,10 +1,13 @@
 package TarjetaDeDuelo;
 
+import Jugador.Jugador;
+import Personaje.Personaje;
+
 @SuppressWarnings("all")
 public class TarjetaDeDuelo {
   private String linea;
   
-  private /* Personaje */Object personaje;
+  private Personaje personaje;
   
-  private /* Jugador */Object jugador;
+  private Jugador jugador;
 }
