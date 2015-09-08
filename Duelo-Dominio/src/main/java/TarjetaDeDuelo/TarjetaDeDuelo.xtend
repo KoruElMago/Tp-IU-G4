c46093c17f5a9b89b10f9e318c125e3560b5afb9
KoruElMago <javier.corbalan93@gmail.com>
2015-09-08 19:25:22 -0300

    creada clase tarjetaDeDuelo

---
+++ Duelo-Dominio/src/main/java/TarjetaDeDuelo/TarjetaDeDuelo.xtend
@@ -0,0 +1,8 @@
+package TarjetaDeDuelo
+
+class TarjetaDeDuelo {
+	
+	var String linea
+	var Personaje personaje
+	var Jugador   jugador
+}
---
+++ Duelo-Dominio/src/main/xtend-gen/TarjetaDeDuelo/TarjetaDeDuelo.java
@@ -0,0 +1,10 @@
+package TarjetaDeDuelo;
+
+@SuppressWarnings("all")
+public class TarjetaDeDuelo {
+  private String linea;
+  
+  private /* Personaje */Object personaje;
+  
+  private /* Jugador */Object jugador;
+}
