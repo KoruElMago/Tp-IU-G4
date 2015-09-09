5d712590acdbee03e4c70ddf866c9ee673e96215
FedePalombo <fede.s.palombo@gmail.com>
2015-09-09 20:17:30 -0300

    asd

--- Duelo-Dominio/src/main/java/Jugador/Jugador.xtend
+++ Duelo-Dominio/src/main/java/Jugador/Jugador.xtend
@@ -1,5 +1,19 @@
 package Jugador
 
+import java.util.List
+import java.util.ArrayList
+
 class Jugador {
+	
+	String nombreJugador
+	int calificacion
+	List<Estadistica> estadisticas
+	
+	
+	new(String nombre){
+	this.nombreJugador = nombre
+	this.calificacion = 0
+	this.estadisticas = new ArrayList<Estadistica>
+	}
 	
 }
--- Duelo-Dominio/src/main/xtend-gen/Jugador/Jugador.java
+++ Duelo-Dominio/src/main/xtend-gen/Jugador/Jugador.java
@@ -1,5 +1,17 @@
 package Jugador;
 
+import java.util.List;
+
 @SuppressWarnings("all")
 public class Jugador {
+  private String nombreJugador;
+  
+  private int calificacion;
+  
+  private /* List<Estadistica> */Object estadisticas;
+  
+  public Jugador(final String nombre) {
+    throw new Error("Unresolved compilation problems:"
+      + "\nEstadistica cannot be resolved to a type.");
+  }
 }
